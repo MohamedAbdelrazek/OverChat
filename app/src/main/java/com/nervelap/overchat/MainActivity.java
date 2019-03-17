18b5d8b3b80846c3bd6ee3723dad5af54fb1ce2c
Mohamed AbdelraZek <muhamedrabi3@gmail.com>
2019-03-17 23:08:50 +0200

    Finisdh designing Login and Register activities ....

--- app/src/main/java/com/nervelap/overchat/MainActivity.java
+++ app/src/main/java/com/nervelap/overchat/MainActivity.java
@@ -1,17 +1,24 @@
 package com.nervelap.overchat;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar mToolbar;
-    ViewPager mViewPager;
-    TabLayout mTabLayout;
-    TapsAdapter mTapsAdapter;
+    private Toolbar mToolbar;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private TapsAdapter mTapsAdapter;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,23 @@ public class MainActivity extends AppCompatActivity {
         mTapsAdapter = new TapsAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(mTapsAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
+        //  FirebaseApp.initializeApp(this);
+        //    mFirebaseAuth = FirebaseAuth.getInstance();
+        //mCurrentUser = mFirebaseAuth.getCurrentUser();
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mCurrentUser == null) {
+
+            sendUserToLoginActivity();
+
+        }
+    }
+
+    private void sendUserToLoginActivity() {
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
