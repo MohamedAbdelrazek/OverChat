ff77be2d0fd89411dcd12d98a2d118602f2fb024
Mohamed AbdelraZek <muhamedrabi3@gmail.com>
2019-03-17 14:42:57 +0200

    adding custom app_bar

--- app/src/main/java/com/nervelap/overchat/MainActivity.java
+++ app/src/main/java/com/nervelap/overchat/MainActivity.java
@@ -2,12 +2,17 @@ package com.nervelap.overchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar mToolbar ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar=findViewById(R.id.main_activity_app_bar_id);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Over Chat");
     }
 }
