9b90dd88c356929da3fc11e6d0fb674b2a45e723
Mohamed AbdelraZek <muhamedrabi3@gmail.com>
2019-03-17 17:35:23 +0200

    Setup Tablayout && Viewpage

--- app/src/main/java/com/nervelap/overchat/MainActivity.java
+++ app/src/main/java/com/nervelap/overchat/MainActivity.java
@@ -1,18 +1,30 @@
 package com.nervelap.overchat;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar mToolbar ;
+    Toolbar mToolbar;
+    ViewPager mViewPager;
+    TabLayout mTabLayout;
+    TapsAdapter mTapsAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mToolbar=findViewById(R.id.main_activity_app_bar_id);
+        mToolbar = findViewById(R.id.main_activity_app_bar_id);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Over Chat");
+        mViewPager = findViewById(R.id.main_tabs_pager);
+        mTabLayout = findViewById(R.id.main_tab_id);
+        mTapsAdapter = new TapsAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mTapsAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+
     }
 }
